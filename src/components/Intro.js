973749ee97023e80fbec5bd2b6a69fa82d0983ee
Ryan Grosser <ryan.grosser91@gmail.com>
2019-01-16 15:23:04 +1000

    Worked on some css styling but is not finished

---
+++ src/components/Intro.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import background from '../images/background.jpg';
+import logo from '../images/logo.png'
+import '../styles/Intro.css'
+
+class Intro extends Component {
+    render() {
+      return (
+        <div className="background">
+            <div className="introBox">
+                <div className="welcomeText">
+                    <h1>Hello, my name is Ryan and welcome to my website.</h1>
+                    <h3>I am an aspiring web developer and you have stumbled onto my portfolio page.</h3>
+                </div>
+                <img src={logo} className="logo" alt="logo" />
+            </div>
+        </div>
+      );
+    }
+  }
+  
+  export default Intro;
