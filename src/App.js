16b3c6d0ae270fe0357f75f1ec78040ad3ced234
Ryan Grosser <ryan.grosser91@gmail.com>
2019-01-17 11:44:49 +1000

    Added about me section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Intro from './components/Intro'
+import AboutMe from './components/AboutMe'
 import './styles/App.css';
 
 class App extends Component {
@@ -7,11 +8,7 @@ class App extends Component {
     return (
       <div>
         <Intro />
-        <div className="backgroundTwo">
-          <div className="briefBox">
-            <h3>About me</h3>
-          </div>
-        </div>
+        <AboutMe />
       </div>
     );
   }
