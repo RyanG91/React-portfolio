19d8141407ff4b616fae0367713a2ac3e732db83
Ryan Grosser <ryan.grosser91@gmail.com>
2019-01-17 11:04:49 +1000

    Removed commented code

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -19,18 +19,6 @@ class Intro extends Component {
         </div>
       );
     }
-  }
+}
 
-// <div class="area1">
-//   <div class="intro">
-//     <section>
-//       <img class="logo" src="images/RG.png" alt="logo">
-//       <div class="typewriter">
-//         <h1> Hello, my name is Ryan and welcome to my website.</h1>
-//         <h2> I am an aspiring web developer and you have stumbled onto my portfolio page.</h2>
-//       </div>
-//     </section>
-//   </div>
-// </div>
-  
   export default Intro;
