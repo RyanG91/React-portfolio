013e7cb0dffb7e742be7bace1328af0157b767a5
Ryan Grosser <ryan.grosser91@gmail.com>
2019-01-24 15:39:41 +1000

    Added style components to Contact section

---
+++ src/styles/ContactStyles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+const Background = styled.div`
+    background-color: #778899;
+    padding-top: 5em;
+    padding-bottom: 5em;
+`
+
+const ContactBox = styled.div`
+    display: flex;
+    font-size: 20px;
+    font-family: 'News Cycle', sans-serif;
+    padding: 2em;
+    padding-top: 5em;
+    padding-bottom: 5em;
+    margin-left: 100px;
+    margin-right: 100px;
+    border-radius: 40px;
+    background-color: white;
+`
+
+export { Background, ContactBox }
