48145dca535dc42723dc1705ec719ad6a12bc5f1
Ryan Grosser <ryan.grosser91@gmail.com>
2019-02-26 15:14:13 +1000

    Minor changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import '../styles/NavBarStyles'
+import { NavBarStyle } from'../styles/NavBarStyles'
+
 
 class NavBar extends Component {
   render() {
     return (
       <div className="NavBar">
-        <nav>
+        <NavBarStyle>
           <a id='tablecolumn' href="/">Home</a>
           <a id='tablecolumn' href="#aboutme">About Me</a>
           <a id='tablecolumn' href="#myprojects">Projects</a>
           <a id='tablecolumn' href="#contact">Contact Me</a>
           <a id='tablecolumn' href="https://github.com/RyanG91" rel="noopener noreferrer" target="_blank">GitHub Page </a>
           <a href="https://www.linkedin.com/in/ryan-grosser-a360a1129/" rel="noopener noreferrer" target="_blank">LinkedIn Page</a>
-        </nav>
+        </NavBarStyle>
       </div>
     );
   }
